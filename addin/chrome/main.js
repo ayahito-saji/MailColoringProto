1a1cecbb1c37867efeaa1f903902a3df198520fa
KOBAYASHI <small1hayashi1m@yahoo.co.jp>
2018-09-23 14:36:01 +0900

    Chrome拡張完成

--- addin/chrome/main.js
+++ addin/chrome/main.js
@@ -1,6 +1,33 @@
-alert("hello");
+var req = new XMLHttpRequest();
+req.onreadystatechange = function() {
+    if (req.readyState == 4 && req.status == 200) { // 通信の完了時
+        //alert("通信できたお");
+//        alert(req.response);
+        document.getElementsByClassName("wide-content-host")[0].innerHTML = req.response;
 
-var text = document.getElementsByClassName("wide-content-host")[0].innerText
+    }else{
+//      alert("通信中。");
+    }
+};
+
+
+
+
+var old_text = "";
+setInterval(function(){
+    var text = document.getElementsByClassName("wide-content-host")[0].innerText;
+    if (text && text != old_text){
+
+        req.open('POST', 'https://64878619.ngrok.io/api', true);
+        req.setRequestHeader('content-type','application/x-www-form-urlencoded;charset=UTF-8');
+        //req.responseType = "document";
+        req.send("text=" + encodeURIComponent(text));
+        //req.abort();
+
+        old_text = text;
+    }
+}, 1000);
+
 
 // var text = document.getElementsByClassName("PlainText")[0].innerHTML;
 // alert(text);
